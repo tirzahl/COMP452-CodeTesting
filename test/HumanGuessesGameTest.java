e25a4bc8e8462119478a5214dd1f31e29e892691
Luke Greenway <greenwaylj18@gcc.edu>
2022-03-16 21:23:03 -0400

    Wrote more tests for HumanGuessesGame

--- test/HumanGuessesGameTest.java
+++ test/HumanGuessesGameTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
+import java.util.Random;
 
 
 public class HumanGuessesGameTest {
@@ -12,6 +13,29 @@ public class HumanGuessesGameTest {
         assertEquals(0, myHGGTest.getNumGuesses());
         assertEquals(false, myHGGTest.isDone());
         assertEquals(500, myHGGTest.getTarget());
+    }
+
+    @Test
+    void guessIncrements(){
+        RandomTestStub myRandTestStub = new RandomTestStub(500);
+        HumanGuessesGame myHGGTest = new HumanGuessesGame(myRandTestStub);
+        assertEquals(0, myHGGTest.getNumGuesses());
+        myHGGTest.makeGuess(1);
+        assertEquals(myHGGTest.getNumGuesses(), 1);
+    }
+
+    @Test
+    void lowGuessReturnsLow(){
+        RandomTestStub myRandTestStub = new RandomTestStub(500);
+        HumanGuessesGame myHGGTest = new HumanGuessesGame(myRandTestStub);
+        assertEquals(GuessResult.LOW, myHGGTest.makeGuess(1));
+    }
+
+    @Test
+    void highGuessReturnsHigh(){
+        RandomTestStub myRandTestStub = new RandomTestStub(500);
+        HumanGuessesGame myHGGTest = new HumanGuessesGame(myRandTestStub);
+        assertEquals(GuessResult.HIGH, myHGGTest.makeGuess(1000));
     }
 
 }
